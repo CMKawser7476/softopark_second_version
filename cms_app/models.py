436d05ca322d5b1ee6cfd2fa782de541f75750e7
Kawser Choudhury <cmkawser747@gmail.com>
2021-06-24 12:06:08 +0600

    heading-logo-border-by-6-add

--- cms_app/models.py
+++ cms_app/models.py
@@ -11,6 +11,7 @@ SECTION_TYPE_CHOICES = [
     ("table_with_tech", "Table With Tech"),
     ("table_with_services", "Table With Services"),
     ("table_with_sercices_2", "Table With Services 2"),
+    ("heading_logo_border_by_6", "Heading Logo Border By 6"),
     ("book_an_appointment", "Book An Appointment"),
     ("call_to_action", "Call To Action"),
     ("call_to_action2", "Call To Action 2"),
