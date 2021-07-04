fb00e1fa65265a4d2ab2b16e8e7b206762989723
Kawser Choudhury <cmkawser747@gmail.com>
2021-07-04 15:11:30 +0600

    cards_4-new-section-add

--- cms_app/views.py
+++ cms_app/views.py
@@ -17,6 +17,7 @@ SECTION_TYPE_CHOICES = [
     ("image_with_cards_by_2", ['heading', 'ordering']),
     ("testimonials", ['heading', 'ordering']),
     ("cards_3", ['heading', 'ordering']),
+    ("cards_4", ['heading', 'ordering']),
     ("all_logos", ['heading', 'ordering']),
     ("video_with_full_container", ['heading', 'ordering']),
     ("video_with_description_by_2", ['heading', 'ordering']),
