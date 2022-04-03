a79cb6d7279bfcde446bd4c1dd03a88c8009db48
Kawser Chowdhury <cmkawser747@gmail.com>
2022-04-03 11:42:14 +0600

    db-up-softopark-cms

--- cms_app/models.py
+++ cms_app/models.py
@@ -167,7 +167,7 @@ class ImageWithDescription(models.Model):
     description = models.TextField( blank=True, null=True)
     # description = HTMLField("Description", null=True, blank=True)
     target_url = models.URLField(blank=True, null=True)
-    button_text = models.CharField(max_length=255, blank=True, null=True)
+    # button_text = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.heading
